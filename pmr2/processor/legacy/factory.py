8a6f687f968728210bfdb767a60bd229469849cc
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-12-07 05:05:09 +0000

    Incorporated dc:title field of the root node.
      - so title can be based off the model rather than some citation in
        some need-to-be deprecated rdf specification.
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.processor.legacy/trunk@4301 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/processor/legacy/factory.py
+++ pmr2/processor/legacy/factory.py
@@ -4,8 +4,9 @@ import zope.interface
 
 from pmr2.processor.cmeta import Cmeta
 
-from pmr2.app.interfaces import IDocViewGen
+from pmr2.app.interfaces import IDocViewGen, IExposure
 from pmr2.app.factory import named_factory, PortalTransformDocViewGenBase
+from pmr2.app.util import simple_valid_date
 
 
 class CellMLTmpDocViewGen(PortalTransformDocViewGenBase):
@@ -19,12 +20,43 @@ class CellMLTmpDocViewGen(PortalTransformDocViewGenBase):
         super(CellMLTmpDocViewGen, self).__init__(*a, **kw)
         self.metadata = Cmeta(StringIO(self.input))
         self.cmetaids = self.metadata.get_cmetaid()
-        self.citation = self.metadata.get_citation(self.cmetaids[0])
+        self.modelinfo = self.metadata.get_dc_vcard_info('')
+        self.citation = None
+        if self.cmetaids:
+            self.citation = self.metadata.get_citation(self.cmetaids[0])
+
+    def findModelTitle(self):
+        if self.modelinfo and self.modelinfo[0]['title']:
+            return self.modelinfo[0]['title']
+        elif self.citation and self.citation[0]['title']:
+            return self.citation[0]['title']
+        else:
+            return self.context.Title()
 
     def generateTitle(self):
-        return self.citation[0]['title']
+        if not IExposure.providedBy(self.context):
+            # normally we just return the citation title
+            return self.findModelTitle()
+
+        # This overrides default like so because of PMR1.
+        if not self.citation:
+            # unchanged
+            # XXX assuming ATCT
+            return self.context.Title()
+
+        # however for the root object, we want the list of authors.
+        issued = self.citation[0]['issued']
+        authors = ', '.join([c['family'] for c in self.citation[0]['creator']])
+        if simple_valid_date(issued):
+            # Got everything.
+            return u'%s, %s' % (authors, issued[:4])
+        else:
+            # We could pull from workspace, but users can fix this on
+            # their own.  Proper citation metadata spec, etc.
+            return u'%s, ' % (authors,)
 
     def generateDescription(self):
-        return self.citation[0]['title']
+        title = self.findModelTitle() or self.context.Description()
+        return title
 
 CellMLTmpDocViewGenFactory = named_factory(CellMLTmpDocViewGen)
